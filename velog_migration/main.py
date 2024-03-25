1236d5d9814b56fdc723dffa81c877b6a3f3aa3c
PCBL <cksgodl1@naver.com>
2024-03-26 00:45:29 +0900

    feat : file write

---
+++ velog_migration/main.py
@@ -0,0 +1,40 @@
+import os
+
+from velog_migration import velog_config
+from velog_migration.get_all_posts_by_username import get_all_posts
+from velog_migration.get_series_by_username import send_graphql_query
+from velog_migration.replace_special_characters import replace_special_characters
+
+# TODO os 환경변수로 이름 받기
+name = "cksgodl"
+# TODO runner 로컬 위치
+repo_path = "C:\\Users\\cksgo\\Desktop\\velog_migration_scripts"
+
+series_list = send_graphql_query(query=velog_config.get_series_query, name=name)
+all_posts = get_all_posts(name)
+
+filtered_posts = []
+
+for series in series_list:
+    filtered_posts = []
+
+    # 시리즈 필터링
+    for post in all_posts:
+        post_series = post.get('series')
+        if post_series and post_series.get('id') == series['id']:
+            filtered_posts.append(post)
+
+    # 폴더 만들기
+    posts_dir = os.path.join(repo_path, series['name'])
+    if not os.path.exists(posts_dir):
+        os.makedirs(posts_dir)
+
+    # 파일 쓰기
+    for post in filtered_posts:
+        file_name = replace_special_characters(post['title']) + '.md'
+        file_path = os.path.join(posts_dir, file_name)
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(post['body'])
+
+    print("--------------")
+
